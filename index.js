32121cbcc7c0a78c2477e52ef06895c6b976da86
Krasimir Tsonev <krasimir@outset.ws>
2018-11-30 12:33:12 +0200

    Trying to get the console args

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-console.log('Hello world');
+const fs = require('fs');
+const path = require('path');
+const program = require('commander');
+const pkg = require('./package.json');
+
+function error(msg) {
+  console.log('\x1b[31m', msg);
+  process.exit(1);
+}
+
+program
+  .version(pkg.version)
+  .option('-c', '--config', 'The path to now.json if it is missing in the current directory')
+  .parse(process.argv);
+
+console.log(program.config);
+const nowConfPath = program.config || path.normalize(process.cwd() + '/now.json');
+
+if (fs.existsSync(nowConfPath)) {
+  const nowConf = require(nowConfPath);
+
+  console.log(nowConf);
+} else {
+  error('now.json can not be found in ' + nowConfPath);
+}
+
+
+
+
+
+
